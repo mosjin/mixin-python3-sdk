e98230fea9af1aadcb3c5cca833ab51585deea7e
mosjin <mosjin@gmail.com>
2019-12-03 19:39:42 +0800

    Added libraries need dir.

---
+++ needLibraries/install_libraries.cmd
@@ -0,0 +1,19 @@
+
+REM crypt : need Microsoft Visual C++ 14.0, can be installed by Windows_visualCppBuildtools_full.exe
+pip install pycrypt  
+
+REM JSON Web Token library for Python 3
+pip install jwt
+
+REM Secure hash and message digest algorithm library
+REM hashlib
+
+REM uuid
+REM json
+REM requests
+REM urllib
+REM base64
+REM time
+REM datetime
+
+
